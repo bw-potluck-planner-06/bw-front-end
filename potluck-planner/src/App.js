ca7f76acc3406bc78727a96f9a966bb61bcf10d9
dominthebox <dsallustro@gmail.com>
2021-08-23 13:08:38 -0400

    first commit, wip

---
+++ potluck-planner/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is the Potluck Planner App! Welcome!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
