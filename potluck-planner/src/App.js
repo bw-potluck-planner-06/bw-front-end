23e6edc7de858bf908fccb3700a07526d134802e
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-08-25 20:51:20 -0400

    Figured out login button with delay
    Co-authored-by: SelfCheckout94 <SelfCheckout94@users.noreply.github.com>

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { Link, Route, Switch } from "react-router-dom";
-
+import ButtonLogin from "./components/Button";
 import Events from "./components/Events";
 import LandingPage from "./components/LandingPage";
 import Login from "./components/Login";
@@ -21,12 +21,16 @@ function App() {
           <Link to="/">Logout</Link>
         </div>
       </nav>
-      <Switch>
-        <Route path="/register" component={Register} />
-        <Route path="/events" component={Events} />
-        <Route path="/login" component={Login} />
-        <Route exact path="/" component={LandingPage} />
-      </Switch>
+
+      <div className="Cards">
+        <Switch>
+          <Route path="/button" component={ButtonLogin} />
+          <Route path="/register" component={Register} />
+          <Route path="/events" component={Events} />
+          <Route path="/login" component={Login} />
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </div>
       {/* <header className="App-header">
         <h1>This is the Potluck Planner App! Welcome!</h1>
       </header> */}
